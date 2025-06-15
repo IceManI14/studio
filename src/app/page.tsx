2fab357be342835e3f20b2aa12fcba9a1a922f4f
Paul Lyman <paull@drinkoptimum.com>
2025-06-15 21:59:30 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <ToastDescription>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -316,7 +316,7 @@ export default function HomePage() {
 
     toast({
       title: "Field Day Ended",
-      description: `Great work ${selectedSalesperson?.name}! You completed ${numberOfVisits} visit${numberOfVisits === 1 ? '' : 's'} today. Your session has been reset. Tomorrow is a new day!`,
+      description: `Great work ${selectedSalesperson?.name}! You have completed ${numberOfVisits} visit${numberOfVisits === 1 ? '' : 's'} today. Your session has been reset. Tomorrow is a new day!`,
     });
     setIsEndDayConfirmOpen(false);
   };
