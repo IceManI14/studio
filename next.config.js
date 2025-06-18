7d7490e1211566025f891eb4ed54e42c41a79c8f
Paul Lyman <paull@drinkoptimum.com>
2025-06-18 22:16:54 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -23,6 +23,22 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer, webpack }) => {
+    // To suppress the "require.extensions is not supported" warning from Handlebars
+    // This warning appears because Handlebars contains code that uses a Node.js-specific feature
+    // not fully supported or emulated by webpack during the bundling process.
+    // For the typical use of Handlebars in Genkit (compiling template strings),
+    // this feature is usually not critical.
+    if (!config.ignoreWarnings) {
+      config.ignoreWarnings = [];
+    }
+    config.ignoreWarnings.push({
+      module: /handlebars\/lib\/index\.js$/, // Regular expression to match the module path
+      message: /require\.extensions is not supported by webpack/, // Regular expression to match the warning message
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
