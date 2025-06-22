ed53c2cbcc7d2072d5ecc23338d384a4b5e81023
Paul Lyman <paull@drinkoptimum.com>
2025-06-22 14:43:55 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <li>
    
    with the tex

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -934,10 +934,10 @@ export default function HomePage() {
                     and potentially help you get to the decision maker more efficiently.
                   </li>
                   <li>
-                    When you "Log the meeting!" the app creates a visit card that stores very important information for your future. The more information you add to this, the more powerful the app becomes. The AI chat will have more ammunition to work with! So be as detailed as you can to become a more optimum salesperson.
+                    When you "Log the meeting!" the app creates a visit card that stores very important information for your future planning. The more information you add to this, the more powerful the app becomes. The AI chat will have more ammunition to work with! So be as detailed as you can to become a more optimum salesperson.
                   </li>
                    <li>
-                    When you "End Day!" the app saves the visit cards you created for the day and tells you how many companies you visited. It then gives you the capability to export your visits as a pdf for easy viewing later. This data can easily be sent to your manager for analysis with the "Email manager" button. Saving you time and aggravation.
+                    When you "End Day!" the app saves the visit cards you created for the day and tells you how many companies you have visited. It then gives you the capability to export your visits as a pdf for easy viewing later. This data can easily be sent to your manager for analysis with the "Email manager" button. Saving you time and aggravation.
                   </li>
                   <li>
                     "Debbie" is your customized AI agent. You will be surprised at her capabilities, and you are encouraged to challenge her. She will only get more effective and more able as time goes by and as you log as much info as you can.
