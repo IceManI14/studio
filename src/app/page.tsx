1072188ced9b0847bac19d2b9967d185e4f44f31
Paul Lyman <paull@drinkoptimum.com>
2025-06-30 04:17:01 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button className=

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -763,7 +763,7 @@ export default function HomePage() {
                     size="sm"
                   >
                     <Map className="mr-2 h-4 w-4" />
-                    Navigate to Suggested Parking
+                    Navigate to AI Destination
                   </Button>
                 )}
                 {isFetchingCity && (
