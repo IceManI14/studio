7c9a3c78f33d78f9622e2d1230c5676d7751fded
Paul Lyman <paull@drinkoptimum.com>
2025-07-21 03:55:39 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
       };
     }
     
+    // This loader handles the 'handlebars' dependency issue with Genkit.
+    config.module.rules.push({
+      test: /node_modules\/handlebars\/.+\.js$/,
+      loader: 'shebang-loader',
+    });
+    
     return config;
   },
 };
